e0b07a4bfe90e7b40c12a305e97d9b50d154b437
ekrasutzky <ekrasutzky@gmail.com>
2023-09-22 19:57:42 +0000

    install colorama

--- run.py
+++ run.py
@@ -1,19 +1,73 @@
 import random
-import colorama
 
-"""
-List of words for the game
-"""
+"""List of words for the game"""
 word_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
 
-"""Select a random word from the list"""
-selected_word = random.choice(word_list)
+""" ASCII art for hangman """
+hangman_stages = [
+    """
+     ------
+    |    |
+         |
+         |
+         |
+         |
+    """,
+    """
+     ------
+    |    |
+    O    |
+         |
+         |
+         |
+    """,
+    """
+     ------
+    |    |
+    O    |
+    |    |
+         |
+         |
+    """,
+    """
+     ------
+    |    |
+    O    |
+   /|    |
+         |
+         |
+    """,
+    """
+     ------
+    |    |
+    O    |
+   /|\\   |
+         |
+         |
+    """,
+    """
+     ------
+    |    |
+    O    |
+   /|\\   |
+   /     |
+         |
+    """,
+    """
+     ------
+    |    |
+    O    |
+   /|\\   |
+   / \\   |
+         |
+    """
+]
 
-"""Initialize variables"""
-guesses = ""
-max_attempts = 6
+"""Function to select a random word from the list"""
+def select_word():
+    return random.choice(word_list)
 
-"""Function to display the current state of the word with underscores for hidden letters"""
+""" Function to display the current state of the word with underscores for hidden letters"""
 def display_word(word, guesses):
     display = ""
     for letter in word:
@@ -23,24 +77,42 @@ def display_word(word, guesses):
             display += "_"
     return display
 
-"""Main game loop"""
-print("Welcome to Hangman!")
-while max_attempts > 0:
-    print(display_word(selected_word, guesses))
-    guess = input("Guess a letter: ").lower()
+""" Function to play the Hangman game"""
+def play_hangman():
+    selected_word = select_word()
+    guesses = ""
+    max_attempts = 6
+    hangman_stage = 0
 
-    if len(guess) != 1 or not guess.isalpha():
-        print("Please enter a single letter.")
-        continue
+    print("Welcome to Hangman!")
 
-    if guess in selected_word:
-        guesses += guess
-        elif display_word(selected_word, guesses) == selected_word:
-            print(f"Congratulations! You guessed the word: {selected_word}")
-            break
-    else:
-        max_attempts -= 1
-        print(f"Wrong guess! You have {max_attempts} attempts left.")
+    while max_attempts > 0:
+        print(hangman_stages[hangman_stage])
+        print(display_word(selected_word, guesses))
+        guess = input("Guess a letter: ").lower()
 
-if max_attempts == 0:
-    print(f"Sorry, you're out of attempts. The word was {selected_word}. Better luck next time!")
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single letter.")
+            continue
+
+        if guess in selected_word:
+            guesses += guess
+            if display_word(selected_word, guesses) == selected_word:
+                print(f"Congratulations! You guessed the word: {selected_word}")
+                break
+        else:
+            max_attempts -= 1
+            hangman_stage += 1
+            print(f"Wrong guess! You have {max_attempts} attempts left.")
+
+    if max_attempts == 0:
+        print(f"Sorry, you're out of attempts. The word was {selected_word}.")
+
+""" Main loop for restarting the game """
+while True:
+    play_hangman()
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again != "yes":
+        break
+
+print("Thanks for playing Hangman!")
