bf27a85a748442e704a0a8420727403fcb408eef
ekrasutzky <ekrasutzky@gmail.com>
2023-09-24 18:22:06 +0000

    fix style

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 import random
 import colorama
 from colorama import Fore, Back, Style
-colorama.init(autoreset= True)
+colorama.init(autoreset=True)
 
 """
 List of words for the game
 """
-word_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
+word_list = ["apple", "banana", "cherry", "date", "grape", "kiwi", "lemon"]
 
-""" 
-ASCII art for hangman 
+"""
+ASCII art for hangman
 """
 hangman_stages = [
     """
@@ -73,12 +73,18 @@ hangman_stages = [
 """
 Function to select a random word from the list
 """
+
+
 def select_word():
     return random.choice(word_list)
 
-""" 
-Function to display the current state of the word with underscores for hidden letters
+
 """
+Function to display the current state of the
+word with underscores for hidden letters
+"""
+
+
 def display_word(word, guesses):
     display = ""
     for letter in word:
@@ -88,16 +94,19 @@ def display_word(word, guesses):
             display += "_"
     return display
 
-""" 
+
+"""
 Function to play the Hangman game
 """
+
+
 def play_hangman():
     selected_word = select_word()
     guesses = ""
     max_attempts = 6
     hangman_stage = 0
 
-    print(Fore.MAGENTA+ "Welcome to Hangman!")
+    print(Fore.MAGENTA + "Welcome to Hangman!")
 
     while max_attempts > 0:
         print(hangman_stages[hangman_stage])
@@ -105,7 +114,7 @@ def play_hangman():
         guess = input("Guess a letter: ").lower()
 
         if len(guess) != 1 or not guess.isalpha():
-            print(Fore.YELLOW+ f"Please enter a single letter.")
+            print(Fore.YELLOW + f"Please enter a single letter.")
             continue
 
         if guess in selected_word:
@@ -116,14 +125,17 @@ def play_hangman():
         else:
             max_attempts -= 1
             hangman_stage += 1
-            print(Back.RED+ f"Wrong guess! You have {max_attempts} attempts left.")
+            print(Back.RED + f"Wrong guess! You have {max_attempts} attempts left.")
 
     if max_attempts == 0:
         print(f"Sorry, you're out of attempts. The word was {Back.MAGENTA}{Fore.CYAN}{Style.BRIGHT} {selected_word}.")
 
-""" 
-Main loop for restarting the game 
+
 """
+Main loop for restarting the game
+"""
+
+
 while True:
     play_hangman()
     play_again = input(f"{Fore.CYAN} Do you want to play again? (yes/no): ").lower()
